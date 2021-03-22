8b404f6933b8ba43d03f564e812e69175dff5f4f
EvaMarBar <martinezbarcelo.e@gmai.com>
2021-03-22 15:55:21 +0100

    Adaptación a sass y modelo por secciones.

---
+++ resources/views/admin/layout/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+
+        <title>Maquetacion</title>
+        
+        @include("admin.layout.partials.style") 
+
+    </head>
+
+    <body>
+
+        <div class="main">
+            @yield('table-form')
+        </div>
+
+    </body>
+
+    @include("admin.layout.partials.js")
+    
+</html>
+        
